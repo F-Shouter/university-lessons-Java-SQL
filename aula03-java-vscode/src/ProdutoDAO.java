79455a31e841cd028e7c79bee338c195be5ed090
F-Shouter <132644949+F-Shouter@users.noreply.github.com>
2024-08-25 14:25:37 -0300

    Code added in ProdutoDAO Class

---
+++ aula03-java-vscode/src/ProdutoDAO.java
@@ -0,0 +1,48 @@
+import java.sql.SQLException;
+import java.util.List;
+
+public class ProdutoDAO {
+    public void cadastrar(Produto p) throws ClassNotFoundException,
+    SQLException {
+    Connection con = Conexao.getConexao();
+    PreparedStatement comando = con.prepareStatement("insert into produtos
+    (descricao, preco) values (?,?)");
+    comando.setString(1, p.getDescricao());
+    comando.setDouble(2, p.getPreco());
+    comando.execute();
+    con.close();
+    }
+    public Produto consultarById(Produto prod) throws ClassNotFoundException,
+    SQLException {
+    Connection con = Conexao.getConexao();
+    PreparedStatement comando = con.prepareStatement("select id, descricao,
+    preco from produtos where id=?");
+    comando.setInt(1, prod.getId());
+    ResultSet resultado = comando.executeQuery();
+    Produto p = new Produto();
+    if (resultado.next()) {
+    p.setId(resultado.getInt("id"));
+    p.setDescricao(resultado.getString("descricao"));
+    p.setPreco(resultado.getDouble("preco"));
+    }
+    con.close();
+    return p;
+    }
+    public List<Produto> consultarTodos() throws ClassNotFoundException,
+    SQLException {
+    Connection con = Conexao.getConexao();
+    PreparedStatement comando = con.prepareStatement("select id, descricao,
+    preco from produtos order by descricao ");
+    ResultSet resultado = comando.executeQuery();
+    List<Produto> todosProdutos = new ArrayList<Produto>();
+    while (resultado.next()) {
+    Produto p = new Produto();
+    p.setId(resultado.getInt("id"));
+    p.setDescricao(resultado.getString("descricao"));
+    p.setPreco(resultado.getDouble("preco"));
+    todosProdutos.add(p);
+    }
+    con.close();
+    return todosProdutos;
+    }
+}
